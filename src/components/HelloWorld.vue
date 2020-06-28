1bb3a8ed4223a28a84ab19f6eb6030c6ca9fe3ca
GaoJiao <2011747643@qq.com>
2020-06-28 16:02:57 +0800

    dev修改了

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,15 @@
   <div class="hello">
     <h3>HelloWorld</h3>
     <h1>{{ msg }}</h1>
+    <samp>
+      我是谁
+      <samp>
+        我在哪
+        <samp>我在干嘛</samp>
+      </samp>
+    </samp>
+    <p>在master上修改代码</p>
+    <p>在dev分支上修改</p>
   </div>
 </template>
 
