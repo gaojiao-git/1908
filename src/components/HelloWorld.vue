868b4ed8285342c50b29b910cf0c5a1bb289e143
gaojiao <2011747643@qq.com>
2020-06-29 15:52:55 +0800

    修改了HelloWorld

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,15 +9,6 @@
         <samp>我在干嘛</samp>
       </samp>
     </samp>
-    <p>在master上修改代码</p>
-    <p>在dev分支上修改</p>
-    <p>{{ msg }}</p>
-    <p>在分支上进行开发：
-      <span>master上的代码</span>
-    </p>
-    <p>在分支上添加代码：
-      <span>master上的代码</span>
-    </p>
   </div>
 </template>
 
